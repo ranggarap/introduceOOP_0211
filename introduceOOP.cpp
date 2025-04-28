98536d2f18463fc204c56ac0c1108719983ccafa
ranggarap <fadhilahrangga26@gmail.com>
2025-04-28 18:38:22 +0700

    masukan function

--- introduceOOP.cpp
+++ introduceOOP.cpp
@@ -1,3 +1,37 @@
 #include <iostream>
 using namespace std;
 
+
+class planet{
+    private:
+            float gravitasi;
+    public:
+        string nama;
+        float diameter;
+
+        void displaydata(){
+            cout << "halo aku planet"<< nama << ", ukuranku" << diameter <<", gravitasiku" << gravitasi<<endl;
+
+        }
+        void inputdata(){
+            cout << "input nama :";
+            cin >> nama;
+            cout << "ukuranku :";
+            cin >> diameter;
+            cout << "gravitasiku :";
+            cin >> gravitasi;
+
+            
+        }
+
+};
+
+int main()
+{
+    planet venus;
+    venus.inputdata();
+    cout << " namanya adalah " << venus.nama << endl;
+    cout << "diameter adalah" << venus.diameter << endl;
+    
+
+}
