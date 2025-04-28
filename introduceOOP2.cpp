0f5040e8077cd3181356fad8c2d1898ad5ce5058
ranggarap <fadhilahrangga26@gmail.com>
2025-04-28 18:56:48 +0700

    masukan method

---
+++ introduceOOP2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class persegipanjang
+{
+public:
+float panjang; 
+float lebar;
+float luas;
+
+void inputdata(){
+    cout << " input panjang :";
+    cin >> panjang;
+    cout << " input lebar :";
+    cin >> lebar;
+    cout << "input lebar:";
+    cin >> lebar;
+}
+    
+
+};
+
+
+
+
+int main(){
+
+
+}
