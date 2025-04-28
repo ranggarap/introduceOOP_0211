52d50b2cf6c1d7d3a9f4a7a7ce1c66f3ebb89dad
ranggarap <fadhilahrangga26@gmail.com>
2025-04-28 18:24:27 +0700

    message input data

--- introduceOOP.cpp
+++ introduceOOP.cpp
@@ -1,24 +1,3 @@
 #include <iostream>
 using namespace std;
 
-//membuat member
-class planet{
-    private:
-            float gravitasi;
-    public:
-        string nama;
-        float diameter;
-
-        void displaydata(){
-            cout << "halo aku planet"<< nama << ", ukuranku" << diameter <<", gravitasiku" << gravitasi<<endl;
-            
-        }
-
-
-};
-
-int main()
-
-{
-    
-}
